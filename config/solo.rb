a15819636f3b5453984a234a803b6e39f988fcc4
Matt Scilipoti <matt@scilipoti.name>
2011-03-25 23:02:51 -0400

    Remove "Permission denied - /var/chef" errors.

--- config/solo.rb
+++ config/solo.rb
@@ -6,10 +6,10 @@ cookbook_path   File.join(root_path, 'cookbooks')
 # Move all the state stuff from /var/chef. I wish there was a single config
 # variable for this!
 
-# state_root_path = File.expand_path('~/.chef/state')
-# file_cache_path  "#{state_root_path}/cache"
-# checksum_path    "#{state_root_path}/checksums"
-# sandbox_path     "#{state_root_path}/sandbox"
-# file_backup_path "#{state_root_path}/backup"
-# cache_options[:path] = file_cache_path
+state_root_path = File.expand_path('~/.chef/state')
+file_cache_path  "#{state_root_path}/cache"
+checksum_path    "#{state_root_path}/checksums"
+sandbox_path     "#{state_root_path}/sandbox"
+file_backup_path "#{state_root_path}/backup"
+cache_options[:path] = file_cache_path
 
